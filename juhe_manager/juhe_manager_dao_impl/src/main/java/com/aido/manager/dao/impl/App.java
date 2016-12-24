7ad1a507134e1a846bbf7b5d90caf67c19441174
double-qiu <934590736@qq.com>
2016-12-24 11:42:46 +0800

    init

---
+++ juhe_manager/juhe_manager_dao_impl/src/main/java/com/aido/manager/dao/impl/App.java
@@ -0,0 +1,13 @@
+package com.aido.manager.dao.impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
