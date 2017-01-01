057f8be8baa30482ed80ba00ade66676e43a52df
double-qiu <934590736@qq.com>
2017-01-01 17:46:39 +0800

    图书商城

---
+++ juhe_portal/juhe_portal_service/src/main/java/com/aido/portal/service/App.java
@@ -0,0 +1,13 @@
+package com.aido.portal.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ juhe_portal/juhe_portal_service/src/main/java/com/aido/portal/service/GoodBookService.java
@@ -0,0 +1,24 @@
+/**  
+ * Project Name:juhe_portal_service  
+ * File Name:GoodBookService.java  
+ * Package Name:com.aido.portal.service  
+ * Date:2017年1月1日17:30:51
+ * Copyright (c) 2016, LoveBeanTec All Rights Reserved.  
+ */
+package com.aido.portal.service;
+
+
+
+/**  
+ * ClassName: GoodBookService  
+ * 聚合数据：图书商城
+ * @author DOUBLE
+ * @version   
+ */
+public interface GoodBookService {
+	
+	//List<> getGoodBookTypeList();
+	
+	
+
+}
